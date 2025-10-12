03deb0c0ff7582877d9f36d7a56c5e1e9f7bbb6b
artem <artem.gaponov.2024@mail.ru>
2025-10-12 11:53:07 +0300

    last

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,14 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-       int balance = 100;
+        int balance = 100;
 
-        int new_balance = 200;
+        int newBalance = 200;
         int result;
-        if (new_balance >= 1000) {
-            result = balance + new_balance + (new_balance / 100);
+        if (newBalance >= 1000) {
+            result = balance + newBalance + (newBalance / 100);
         } else {
-            result = balance + new_balance;
+            result = balance + newBalance;
 
         }
         System.out.println((result));
